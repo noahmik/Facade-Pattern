1bddd7e6da149e00b2819b3bc59644d5fa256601
noahmik <s46422574@gmail.com>
2024-07-20 01:57:34 +0900

    FEAT :: Book,Member 엔티티 추가

---
+++ src/main/java/com/example/FacadeTest/domain/Book.java
@@ -0,0 +1,20 @@
+package com.example.FacadeTest.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+}
---
+++ src/main/java/com/example/FacadeTest/domain/Member.java
@@ -0,0 +1,22 @@
+package com.example.FacadeTest.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+}
