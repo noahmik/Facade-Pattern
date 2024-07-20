9d42adc0c125734109c2dedfbc052cab021e73e7
noahmik <s46422574@gmail.com>
2024-07-21 02:24:53 +0900

    FIX :: cors에러 해결

---
+++ src/main/java/com/example/FacadeTest/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.FacadeTest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:63342")  // 클라이언트가 실행되는 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
