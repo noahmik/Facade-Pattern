89a78b63d7f20f37399f3e34187273ad9e651697
noahmik <s46422574@gmail.com>
2024-07-22 01:48:49 +0900

    FEAT :: 의존성 추가

---
+++ src/main/java/com/example/FacadeTest/FacadeTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.FacadeTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacadeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacadeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FacadeTest/FacadeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FacadeTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacadeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
